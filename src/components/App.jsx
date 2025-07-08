bcc25d1b7c5d97eb9e33d28c6d5ebc01e3e9e959
Jeremy Marube <jeremymarube1@gmail.com>
2025-07-08 16:46:36 +0300

    Adding content

---
+++ src/components/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+
+const user = {
+  name: "Liza",
+  city: "New York",
+  bio: "I made this!",
+  color: "firebrick",
+  links: {
+    github: "https://github.com/liza",
+    linkedin: "https://www.linkedin.com/in/liza/"
+  }
+};
+
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home 
+        name={user.name}
+        city={user.city}
+        color={user.color}
+      />
+      <About 
+        bio={user.bio}
+        github={user.links.github}
+        linkedin={user.links.linkedin}
+      />
+    </div>
+  );
+}
+
+export default App;
